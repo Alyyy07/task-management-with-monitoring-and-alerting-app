2868fcefc59f44963701028e24388c807a1d035e
Alyyy07 <alielhamied1@gmail.com>
2025-12-17 15:43:35 +0700

    [FEAT] add user routes

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,5 +1,10 @@
 import fastify from "fastify";
 import { configPlugin } from "./libs/config.js";
+import { prisma } from "./libs/prisma.js";
+import { authenticate } from "./plugins/authenticate.js";
+import { jwtPlugin } from "./plugins/jwt.js";
+import { authRoutes } from "./modules/auth/auth.routes.js";
+import { userRoutes } from "./modules/user/user.routes.js";
 
 const app = fastify({
   logger: {
@@ -8,10 +13,20 @@ const app = fastify({
 });
 
 app.register(configPlugin);
+app.register(jwtPlugin);
+app.register(authenticate);
 
 app.get("/", async (request, reply) => {
   return { hello: "world" };
 });
+
+app.get("/db-test", async (request, reply) => {
+  const users = await prisma.user.findMany();
+  return { users };
+});
+
+app.register(authRoutes, { prefix: "/auth" });
+app.register(userRoutes, { prefix: "/users" });
 
 app.setErrorHandler((error, request, reply) => {
   request.log.error(error);
