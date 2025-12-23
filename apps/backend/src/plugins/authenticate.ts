d2a4acfaa73d78077f6707a189ea08245b084271
Alyyy07 <alielhamied1@gmail.com>
2025-12-23 15:37:13 +0700

    [FIX] modify authenticate middleware

--- apps/backend/src/plugins/authenticate.ts
+++ apps/backend/src/plugins/authenticate.ts
@@ -1,15 +1,26 @@
 import fp from "fastify-plugin";
-import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyPluginAsync, FastifyRequest } from "fastify";
+import { AuthError, AuthErrorCode } from "../modules/auth/auth.errors.js";
 
-export const authenticate = fp(async (app) => {
-  app.decorate(
-    "authenticate",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      try {
-        await request.jwtVerify();
-      } catch {
-        reply.status(401).send({ message: "Unauthorized" });
-      }
+export const authenticate: FastifyPluginAsync = fp(async (app) => {
+  app.decorate("authenticate", async (req: FastifyRequest) => {
+    const auth = req.headers.authorization;
+
+    if (!auth) {
+      throw new AuthError(AuthErrorCode.NO_TOKEN);
     }
-  );
+
+    const [, token] = auth.split(" ");
+
+    if (!token) {
+      throw new AuthError(AuthErrorCode.NO_TOKEN);
+    }
+
+    try {
+      const payload = app.jwt.verify<{ userId: string }>(token);
+      req.user = { userId: payload.userId };
+    } catch {
+      throw new AuthError(AuthErrorCode.INVALID_ACCESS_TOKEN);
+    }
+  });
 });
