e8455e4fda72d7bfbc2740abd581aab8c1f5dfaf
Alyyy07 <alielhamied1@gmail.com>
2025-12-27 23:38:36 +0700

    [FIX] add validation error for routes

--- apps/backend/src/plugins/error-handler.plugin.ts
+++ apps/backend/src/plugins/error-handler.plugin.ts
@@ -1,20 +1,29 @@
-import { FastifyError, FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply } from "fastify";
 import { AuthError, AuthErrorCode } from "../modules/auth/auth.errors.js";
 
 export function registerErrorHandler(app: FastifyInstance) {
-  app.setErrorHandler((error, _req, reply) => {
+  app.setErrorHandler((error: unknown, _req, reply: FastifyReply) => {
     if (error instanceof AuthError) {
       return handleAuthError(error, reply);
     }
 
-    app.log.error(error);
+    if (typeof error === "object" && error !== null && "validation" in error) {
+      // `validation` comes from AJV/fastify schema validation errors
+      const details = (error as any).validation;
+      return reply.status(400).send({
+        error: "VALIDATION_ERROR",
+        details: details.map((d: any) => ({ message: d.message})),
+      });
+    }
+    
+    app.log.error(error as Error | string);
     reply.status(500).send({
       error: "INTERNAL_SERVER_ERROR",
     });
   });
 }
 
-function handleAuthError(error: AuthError, reply: any) {
+function handleAuthError(error: AuthError, reply: FastifyReply) {
   switch (error.code) {
     case AuthErrorCode.USER_EXISTS:
       return reply.status(409).send({ error: error.code });
