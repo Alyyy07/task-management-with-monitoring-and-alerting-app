dcb23a62b62a798b6aae675c32c9fca5dc04f82c
Alyyy07 <alielhamied1@gmail.com>
2025-12-29 15:04:33 +0700

    [FIX] refactor error handler

--- apps/backend/src/modules/authz/authz.errors.ts
+++ apps/backend/src/modules/authz/authz.errors.ts
@@ -1,8 +1,14 @@
 export enum AuthzErrorCode {
-  FORBIDDEN = "FORBIDDEN",
   NOT_OWNER = "NOT_OWNER",
   NOT_MEMBER = "NOT_MEMBER",
+  NOT_FOUND = "NOT_FOUND",
 }
+
+export const AuthzErrorStatus: Record<AuthzErrorCode, number> = {
+  [AuthzErrorCode.NOT_OWNER]: 403,
+  [AuthzErrorCode.NOT_MEMBER]: 403,
+  [AuthzErrorCode.NOT_FOUND]: 404,
+};
 
 export class AuthzError extends Error {
   constructor(public readonly code: AuthzErrorCode) {
