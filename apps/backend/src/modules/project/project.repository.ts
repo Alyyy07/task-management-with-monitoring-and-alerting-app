c7de5de5437d10ee13e504683e1e93580a0548cb
Alyyy07 <alielhamied1@gmail.com>
2026-01-01 14:56:44 +0700

    [FEAT] add complete CRUD for Project table with proper Authorization based on Role

--- apps/backend/src/modules/project/project.repository.ts
+++ apps/backend/src/modules/project/project.repository.ts
@@ -1,14 +1,39 @@
+// project/project.repository.ts
 import { prisma } from "../../libs/prisma.js";
-import { ProjectRepository } from "./project.types.js";
 
-export const projectRepository: ProjectRepository = {
-  async getProjectWithOrg(projectId: string) {
-    return prisma.project.findUnique({
+export const projectRepository = {
+  async create(data:{
+    name: string;
+    organizationId: string;
+    createdBy: string;
+  }) {
+    return prisma.project.create({ data });
+  },
+
+  async findById(id: string) {
+    return prisma.project.findUnique({ where: { id } });
+  },
+
+  async findByOrganization(organizationId: string) {
+    return prisma.project.findMany({ where: { organizationId } });
+  },
+
+  async update(id: string, data: { name?: string }) {
+    return prisma.project.update({ where: { id }, data });
+  },
+
+  async delete(id: string) {
+    await prisma.project.delete({ where: { id } });
+  },
+
+  async isCreator(projectId: string, userId: string) {
+    const project = await prisma.project.findUnique({
       where: { id: projectId },
-      select: {
-        id: true,
-        organizationId: true,
-      },
+      select: { createdBy: true },
     });
+
+    if (!project) return "NOT_FOUND";
+    if (project.createdBy !== userId) return "NOT_CREATOR";
+    return "CREATOR";
   },
 };
