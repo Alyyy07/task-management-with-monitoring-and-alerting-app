3ae4950935aafa48a9ea75944c6380e5f4e9039e
Alyyy07 <alielhamied1@gmail.com>
2026-01-19 22:29:57 +0700

    [FIX] DATABASE_URL not found

--- apps/backend/src/libs/config.ts
+++ apps/backend/src/libs/config.ts
@@ -1,17 +1,18 @@
-import fp from 'fastify-plugin';
-import env from '@fastify/env';
+import fp from "fastify-plugin";
+import env from "@fastify/env";
 
 const schema = {
   type: "object",
-  required: ["PORT"],
+  required: ["PORT", "DATABASE_URL"],
   properties: {
-    PORT: { type: "number", default: 3000 }
-  }
+    PORT: { type: "number", default: 3000 },
+    DATABASE_URL: { type: "string" },
+  },
 };
 
 export const configPlugin = fp(async (app) => {
   await app.register(env, {
     schema,
-    dotenv: true
+    dotenv: true,
   });
 });
